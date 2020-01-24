24655b62eac7ca2d1d616013a7b239bca28a6cfc
stillsen <stillsen@gmail.com>
2020-01-24 10:03:49 +0100

    initial code

--- basic_analysis.R
+++ basic_analysis.R
@@ -3,3 +3,12 @@
 # Created by: stillsen
 # Created on: 1/22/20
 
+path <- '/home/stillsen/Documents/Uni/HiWi/Source/Drug_Analysis'
+filename <- 'reordered.csv'
+abs_filename <- paste(path, '/', filename, sep='')
+
+# read csv, which is actually tsv ^^
+df <- read.table(abs_filename, sep='\t', header=TRUE)
+# forget about zero filled first column
+df <- df[,-1]
+
