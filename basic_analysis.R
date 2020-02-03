be72808f9ddac5e62bc92196c7f4e3656b253d67
stillsen <stillsen@gmail.com>
2020-02-03 10:08:02 +0100

    figure added

--- basic_analysis.R
+++ basic_analysis.R
@@ -30,44 +30,54 @@ drugs <- c('AMP', 'CPR', 'DOX', 'ERY', 'FOX', 'FUS', 'STR', 'TMP')
 mean_vec <- c()
 stde_vec <- c()
 drug_vec <- c()
+lvls = c(1,2,3)
 
-for (drug in drugs){
-  print(drug)
-  attach(df)
-  # cerate sub dataframe respectively
-  switch(drug,
-         'AMP'={sub_df <- df[which(AMP==1 & is.na(CPR) & is.na(DOX) & is.na(ERY) & is.na(FOX) & is.na(FUS) & is.na(STR) & is.na(TMP)),]},
-         'CPR'={sub_df <- df[which(is.na(AMP) & CPR==1 & is.na(DOX) & is.na(ERY) & is.na(FOX) & is.na(FUS) & is.na(STR) & is.na(TMP)),]},
-         'DOX'={sub_df <- df[which(is.na(AMP) & is.na(CPR) & DOX==1 & is.na(ERY) & is.na(FOX) & is.na(FUS) & is.na(STR) & is.na(TMP)),]},
-         'ERY'={sub_df <- df[which(is.na(AMP) & is.na(CPR) & is.na(DOX) & ERY==1 & is.na(FOX) & is.na(FUS) & is.na(STR) & is.na(TMP)),]},
-         'FOX'={sub_df <- df[which(is.na(AMP) & is.na(CPR) & is.na(DOX) & is.na(ERY) & FOX==1 & is.na(FUS) & is.na(STR) & is.na(TMP)),]},
-         'FUS'={sub_df <- df[which(is.na(AMP) & is.na(CPR) & is.na(DOX) & is.na(ERY) & is.na(FOX) & FUS==1 & is.na(STR) & is.na(TMP)),]},
-         'STR'={sub_df <- df[which(is.na(AMP) & is.na(CPR) & is.na(DOX) & is.na(ERY) & is.na(FOX) & is.na(FUS) & STR==1 & is.na(TMP)),]},
-         'TMP'={sub_df <- df[which(is.na(AMP) & is.na(CPR) & is.na(DOX) & is.na(ERY) & is.na(FOX) & is.na(FUS) & is.na(STR) & TMP==1),]},
-          {print('default')}
-  )
-  # convert it's fake char column to numeric
-  detach(df)
-  sub_df <- transform(sub_df, trial_3=as.numeric(sub_df$trial_3))
-  # compute mean and std error
-  mean_t1 <- mean(sub_df$trial_1)
-  mean_t2 <- mean(sub_df$trial_2)
-  mean_t3 <- mean(sub_df$trial_3)
-  stde_t1 <- std.error(sub_df$trial_1)
-  stde_t2 <- std.error(sub_df$trial_2)
-  stde_t3 <- std.error(sub_df$trial_3)
-  # add to pooling vector
-  mean_vec <- c(mean_vec, mean_t1, mean_t2, mean_t3)
-  stde_vec <- c(stde_vec, stde_t1, stde_t2, stde_t3)
-  drug_vec <- c(drug_vec, drug, drug, drug)
+# mean_lvl <- list()
+# stde_lvl <- list()
+mean_lvl <- c()
+stde_lvl <- c()
+lvl_vec <- c()
+for (i in lvls){
+  for (drug in drugs){
+    print(i)
+    print(drug)
+    attach(df)
+    # cerate sub dataframe respectively
+    switch(drug,
+           'AMP'={sub_df <- df[which(AMP==i & is.na(CPR) & is.na(DOX) & is.na(ERY) & is.na(FOX) & is.na(FUS) & is.na(STR) & is.na(TMP)),]},
+           'CPR'={sub_df <- df[which(is.na(AMP) & CPR==i & is.na(DOX) & is.na(ERY) & is.na(FOX) & is.na(FUS) & is.na(STR) & is.na(TMP)),]},
+           'DOX'={sub_df <- df[which(is.na(AMP) & is.na(CPR) & DOX==i & is.na(ERY) & is.na(FOX) & is.na(FUS) & is.na(STR) & is.na(TMP)),]},
+           'ERY'={sub_df <- df[which(is.na(AMP) & is.na(CPR) & is.na(DOX) & ERY==i & is.na(FOX) & is.na(FUS) & is.na(STR) & is.na(TMP)),]},
+           'FOX'={sub_df <- df[which(is.na(AMP) & is.na(CPR) & is.na(DOX) & is.na(ERY) & FOX==i & is.na(FUS) & is.na(STR) & is.na(TMP)),]},
+           'FUS'={sub_df <- df[which(is.na(AMP) & is.na(CPR) & is.na(DOX) & is.na(ERY) & is.na(FOX) & FUS==i & is.na(STR) & is.na(TMP)),]},
+           'STR'={sub_df <- df[which(is.na(AMP) & is.na(CPR) & is.na(DOX) & is.na(ERY) & is.na(FOX) & is.na(FUS) & STR==i & is.na(TMP)),]},
+           'TMP'={sub_df <- df[which(is.na(AMP) & is.na(CPR) & is.na(DOX) & is.na(ERY) & is.na(FOX) & is.na(FUS) & is.na(STR) & TMP==i),]},
+            {print('default')}
+    )
+    # convert it's fake char column to numeric
+    detach(df)
+    sub_df <- transform(sub_df, trial_3=as.numeric(sub_df$trial_3))
+    # compute mean and std error over all trials for a certain druf and add it to a vector
+    mean_vec <- append(mean_vec, mean( append(append(sub_df$trial_1, sub_df$trial_2), (sub_df$trial_3)) ))
+    stde_vec <- append(stde_vec, std.error( append(append(sub_df$trial_1, sub_df$trial_2), sub_df$trial_3) ))
+  }
+  mean_lvl <- append(mean_lvl,mean_vec)
+  stde_lvl <- append(stde_lvl, stde_vec)
+  lvl_vec <- append(lvl_vec, rep(i, length(drugs)))
+  mean_vec <- c()
+  stde_vec <- c()
 }
 
 # wrapping all up
-result_df <- data.frame('mean' = mean_vec, 'std_error' = stde_vec, 'drug' = drug_vec, stringsAsFactors = FALSE)
+result_df <- data.frame('mean' = mean_lvl,
+                        'std_error' = stde_lvl,
+                        'lvl'=lvl_vec,
+                        'drug' = rep(drugs,3),
+                        stringsAsFactors = FALSE)
+print(result_df)
 detach(df)
 attach(result_df)
 
 ggplot(data = result_df) +
-    #geom_errorbar(aes(ymin=mean-std_error, ymax=mean+std_error), width=.1) +
-    #geom_line() +
-    geom_point(mapping = aes(x=mean, y=drug, colour=drug))
+  geom_errorbar(mapping = aes(ymin=mean-std_error, ymax=mean+std_error, x=drug, group=lvl), position =position_dodge(width = .5),width=.1) +
+  geom_point(mapping = aes(y=mean, x=drug, colour=drug, group=lvl), position =position_dodge(width = .5))
